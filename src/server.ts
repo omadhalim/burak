bf0defb4a98faea4459fd87f99465bf5fc6de6ae
omadhalim <omadjonzokirjonov977@gmail.com>
2026-09-23 12:26:35 +0900

    feat:integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -10,3 +10,10 @@
 // Architectural pattern: MVC, Dependency Injection, MVP
 
 // Design pattern: Middleware, Decotar
+
+import dotenv from "dotenv";
+dotenv.config();
+
+// console.log("PORT:", process.env.PORT);
+
+// console.log("MONGO_URL:",process.env.MONGO_URL);
