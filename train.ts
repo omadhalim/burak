b3c27a7bd29bf31a5153c7fd280db543b388d591
omadhalim <omadjonzokirjonov977@gmail.com>
2026-09-23 13:46:00 +0900

    feat: MIT TASK-M

--- train.ts
+++ train.ts
@@ -1,9 +1,22 @@
-// Task - L
-function reverseSentence(str: string) {
-  return str
-    .split(" ")
-    .map((word: string) => word.split("").reverse().join(""))
-    .join(" ");
+// Task - M
+
+function getSquareNumbers(arr: number[]) {
+  return arr.map(number => {
+    return {
+      number: number,
+      square: number * number
+    };
+  });
 }
 
-console.log(reverseSentence("we like coding!"));
+console.log(getSquareNumbers([1, 2, 3]));
+
+// Task - L
+// function reverseSentence(str: string) {
+//   return str
+//     .split(" ")
+//     .map((word: string) => word.split("").reverse().join(""))
+//     .join(" ");
+// }
+
+// console.log(reverseSentence("we like coding!"));
