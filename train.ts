524d7ba17db1ea9b2b5806277854120c3f665150
omadhalim <omadjonzokirjonov977@gmail.com>
2026-09-25 15:37:38 +0900

    feat: MIT TASK - N

--- train.ts
+++ train.ts
@@ -1,15 +1,28 @@
-// Task - M
+// Task - N
 
-function getSquareNumbers(arr: number[]) {
-  return arr.map(number => {
-    return {
-      number: number,
-      square: number * number
-    };
-  });
+function palindromCheck(str: string) {
+  let reversed = str.split("").reverse().join("");
+
+  return str === reversed;
 }
 
-console.log(getSquareNumbers([1, 2, 3]));
+console.log(palindromCheck("dad"));
+console.log(palindromCheck("hello"));
+
+
+
+// Task - M
+
+// function getSquareNumbers(arr: number[]) {
+//   return arr.map(number => {
+//     return {
+//       number: number,
+//       square: number * number
+//     };
+//   });
+// }
+
+// console.log(getSquareNumbers([1, 2, 3]));
 
 // Task - L
 // function reverseSentence(str: string) {
